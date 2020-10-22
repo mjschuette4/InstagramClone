92f645f1713a1b7d22abb3666c4b173dbdef8e18
Matthew J Schuette <mjschuette4@gmail.com>
2020-10-22 15:11:21 -0500

    Built out header component with logo and began format for posts

--- instagramclone/src/App.js
+++ instagramclone/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Post from "./components/Post";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello there</h1>
+      <div className="app_header">
+        <img
+          className="app_headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+          alt=""
+        />
+        <Post />
+      </div>
     </div>
   );
 }
---
+++ instagramclone/src/components/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Post.css';
+
+function post() {
+    return (
+        <div>
+            <h3>Username</h3>
+            <img className="post_image" src=" "/>
+            <h4>Username: caption</h4>
+        </div>
+    )
+}
+
+export default post
