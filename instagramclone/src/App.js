77a5bc1fd27c862e66ad6e9f02127b73610c62c2
Matthew J Schuette <mjschuette4@gmail.com>
2020-10-22 16:44:59 -0500

    added usestate to better manage posts

--- instagramclone/src/App.js
+++ instagramclone/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Post from "./components/Post.js";
 
 function App() {
+  const [posts,setPosts] = useState([
+    {
+      username: "Matthew",
+      caption: "Wow im incredible",
+      imageUrl: "https://images.unsplash.com/photo-1601306418939-19ee7ccffdf7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
+    },
+    {
+      username: "Sarah",
+      caption: "Wow im incredible",
+      imageUrl: "https://images.unsplash.com/photo-1601306418939-19ee7ccffdf7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
+    }
+  ]);  
   return (
     <div className="App">
       <div className="app_header">
@@ -11,11 +23,12 @@ function App() {
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
           alt=""
         />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+        </div>
+        {
+          posts.map(post => (
+            <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+          ))
+        }
     </div>
   );
 }
--- instagramclone/src/components/Post.js
+++ instagramclone/src/components/Post.js
@@ -2,20 +2,20 @@ import React from 'react';
 import './Post.css';
 import Avatar from "@material-ui/core/Avatar"
 
-function post() {
+function post({username, caption, imageUrl}) {
     return (
         <div className="post">
             <div className="post_header">
                 <Avatar 
                     className="post_avatar"
-                    alt="{username}"
+                    alt={username}
                     src="/static/images/avatar/1.jpg"
                     />
-                <h3>Username</h3>
+                <h3>{username}</h3>
             </div>
             
-            <img className="post_image" src="https://images.unsplash.com/photo-1601306418939-19ee7ccffdf7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"/>
-            <h4 className="post_text"><strong>Username:</strong> caption</h4>
+            <img className="post_image" src={imageUrl}/>
+            <h4 className="post_text"><strong>{username}</strong>{caption}</h4>
         </div>
     )
 }
