dc5ccc3dad396a429bc88106fd2b8875e032b8c3
Matthew J Schuette <mjschuette4@gmail.com>
2020-10-26 12:32:07 -0500

    Modal shows up

--- instagramclone/src/App.js
+++ instagramclone/src/App.js
@@ -1,11 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from "./components/Post.js";
-import { db } from "./Config/Firebase"
-import Modal from "@material-ui/core/Modal"
+import { db } from "./Config/Firebase";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from "@material-ui/core/Modal";
+import { Button } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const [posts,setPosts] = useState([]);  
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
+  const [posts,setPosts] = useState([]);
+  const [open,  setOpen] = useState(false);  
 
   // UseEffect -> runs a piece of code based on a specific condition
 
@@ -19,13 +47,19 @@ function App() {
     });
   }, [])
 
+  const signUp = (event) => {
+
+  }
+
   return (
     <div className="App">
       <Modal
         open={open}
-        onClose={handleClose}
+        onClose={() => setOpen(false)}
       >
-        {body}
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am Modal</h2>
+        </div>
       </Modal>
       <div className="app_header">
         <img
@@ -34,6 +68,7 @@ function App() {
           alt=""
         />
         </div>
+        <Button onClick={() => setOpen(true)}>Sign up</Button>
         {
           posts.map(({id, post}) => (
             <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
