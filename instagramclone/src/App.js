e7476bb7974789ede8f754f1184fc4136ec9b1ad
Matthew J Schuette <mjschuette4@gmail.com>
2020-10-26 12:42:43 -0500

    added inputs/ form to modal

--- instagramclone/src/App.js
+++ instagramclone/src/App.js
@@ -4,7 +4,7 @@ import Post from "./components/Post.js";
 import { db } from "./Config/Firebase";
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from "@material-ui/core/Modal";
-import { Button } from '@material-ui/core';
+import { Button, Input } from '@material-ui/core';
 
 function getModalStyle() {
   const top = 50;
@@ -34,6 +34,9 @@ function App() {
 
   const [posts,setPosts] = useState([]);
   const [open,  setOpen] = useState(false);  
+  const [username,  setUsername] = useState(''); 
+  const [email,  setEmail] = useState(''); 
+  const [password,  setPassword] = useState(''); 
 
   // UseEffect -> runs a piece of code based on a specific condition
 
@@ -58,7 +61,34 @@ function App() {
         onClose={() => setOpen(false)}
       >
         <div style={modalStyle} className={classes.paper}>
-          <h2>I am Modal</h2>
+          <form>
+            <center>
+              <img
+              className="app_headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+              alt=""
+              />
+              <Input
+                placeholder="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Input
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input 
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button onClick={signUp}>Sign up</Button>
+            </center>
+          </form>
         </div>
       </Modal>
       <div className="app_header">
