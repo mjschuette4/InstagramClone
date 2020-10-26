1acd6be213fbe6610fee5ed9f744f4eae07c7360
Matthew J Schuette <mjschuette4@gmail.com>
2020-10-26 13:20:41 -0500

    Sign up auth functions

--- instagramclone/src/App.js
+++ instagramclone/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from "./components/Post.js";
-import { db } from "./Config/Firebase";
+import { db, auth } from "./Config/Firebase";
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from "@material-ui/core/Modal";
 import { Button, Input } from '@material-ui/core';
@@ -31,12 +31,39 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
-
   const [posts,setPosts] = useState([]);
   const [open,  setOpen] = useState(false);  
   const [username,  setUsername] = useState(''); 
   const [email,  setEmail] = useState(''); 
   const [password,  setPassword] = useState(''); 
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in
+        console.log(authUser)
+        setUser(authUser);
+        if(authUser.displayName) {
+          //dont update username
+        } else {
+          // in case we just made someone
+          return authUser.updateProfile({
+            displayName: username,
+          });
+        }
+      } else {
+      // user has logged out
+      setUsername(null);
+      }
+    })
+
+    return () => {
+      //perform some cleanup actions
+      unsubscribe();
+    }
+  }  
+  , [user, username])
 
   // UseEffect -> runs a piece of code based on a specific condition
 
@@ -51,7 +78,16 @@ function App() {
   }, [])
 
   const signUp = (event) => {
+    event.preventDefault();
 
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => alert(error.message));
   }
 
   return (
@@ -61,33 +97,33 @@ function App() {
         onClose={() => setOpen(false)}
       >
         <div style={modalStyle} className={classes.paper}>
-          <form>
+          <form className="app_signup">
             <center>
               <img
               className="app_headerImage"
               src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
               alt=""
               />
-              <Input
-                placeholder="username"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <Input
-                placeholder="email"
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Input 
-              placeholder="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button onClick={signUp}>Sign up</Button>
             </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input 
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign up</Button>          
           </form>
         </div>
       </Modal>
