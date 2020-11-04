de9166c70519058316fa76bb7ef1d1907c9d4f47
Matthew J Schuette <mjschuette4@gmail.com>
2020-11-04 14:03:15 -0600

    Created format for ImageUplaod file

---
+++ instagramclone/src/ImageUpload.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Button } from "@material-ui/core";
+
+function ImageUpload() {
+    const [caption, setCaption] = useState('')
+    const [image, setImage] = useState(null)
+    const [url, setUrl] = useState('')
+    const [progress, setProgress] = useState(0)
+
+    const handleChange = (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0])
+        }
+    };
+
+    const handleUpload = () => {
+
+    };
+
+    return (
+        <div>    
+            <input type="text" placeholder='Enter a caption...' onChange={event => setCaption(event.target.value)} value={caption}/>
+            <input type="file" onChange={handleChange}/>
+            <Button onClick={handleUpload}>
+                Upload
+            </Button>
+        </div>
+    )
+}
+
+export default ImageUpload
